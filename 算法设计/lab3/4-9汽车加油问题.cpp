6a002c5760013903b2c600871dd8f9f07bd88593
Tswatery <1601244044@qq.com>
2022-12-15 13:21:51 +0800

    lab3 finish

--- 算法设计/lab3/4-9汽车加油问题.cpp
+++ 算法设计/lab3/4-9汽车加油问题.cpp
@@ -1,11 +1,24 @@
 #include <bits/stdc++.h>
+#include <windows.h>
 
 using namespace std;
+#define int long long
 
-const int N = 1e5 + 10;
+const int N = 1e7 + 10;
 int a[N];
 
-int main(){
+signed main(){
+    freopen("test.txt", "r", stdin);
+    freopen("out1.txt", "w", stdout);
+
+    
+    LARGE_INTEGER nFreq;
+    LARGE_INTEGER nBeginTime;
+    LARGE_INTEGER nEndTime;
+    double time;
+
+    QueryPerformanceFrequency(&nFreq);
+    QueryPerformanceCounter(&nBeginTime);
     int n, k; cin >> n >> k;
     for(int i = 0; i <= k; ++ i) cin >> a[i];
 
@@ -22,4 +35,12 @@ int main(){
     }
     if(res == -1) cout << "No Solution\n";
     else cout << res << endl;
+
+    QueryPerformanceCounter(&nEndTime);
+    time = (double)(nEndTime.QuadPart - nBeginTime.QuadPart) / (double)nFreq.QuadPart;
+
+	cout << endl;
+
+    cout << "timeÎª£º"<< time;
+
 }
